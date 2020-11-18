fed99da80524bfc00e60edd8db3157be034c474d
Kevin Maykon Batala Santos <kevinoukv123@gmail.com>
2020-11-18 15:48:02 -0300

    correcao de bug no serach persistente da entrega passada

--- comandos/ComandoSearch.java
+++ comandos/ComandoSearch.java
@@ -22,14 +22,14 @@ public class ComandoSearch implements Funcao{
                     resComando[1] = resComando[1].substring(0, resComando[1].length()-1);
                 }
                 Arquivo dir = arvore.interpretaPath(resComando[1]);
-                ((Diretorio)dir).getDir().procuraChave(resComando[1], resComando[2]);
+                ((Diretorio)dir).getDir().procuraChave("./" + resComando[1], resComando[2]);
             }
         }else if(resComando.length == 4){
             if(resComando[2].endsWith("/")){
                     resComando[2] = resComando[2].substring(0, resComando[2].length()-1);
                 }
             Arquivo dir = arvore.interpretaPath(resComando[2]);
-            ((Diretorio)dir).getDir().procuraChaveRec(resComando[2],"", resComando[3]);
+            ((Diretorio)dir).getDir().procuraChaveRec("./" + resComando[2],"", resComando[3]);
         }else{
             throw new FaltaOperandoException(resComando[0]);
         }
