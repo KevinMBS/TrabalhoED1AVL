d1fcfb692071f34a2bba57b35e9025489baba664
Kevin Maykon Batala Santos <kevinoukv123@gmail.com>
2020-11-18 22:56:21 -0300

    correção bug no ls -R

--- elementos/ArvoreAVL.java
+++ elementos/ArvoreAVL.java
@@ -170,14 +170,16 @@ public class ArvoreAVL {
     public static void printArvoreRecursivo(String caminho, ArvoreAVL arvore, String pasta){
         Arquivo raiz = arvore.getRaiz();
         ArrayList<Diretorio> dirsAvisitar = new ArrayList<>();
-        if(!pasta.equals("")){
-            if(pasta.equals(" ")){
-                pasta = "";
-            }
+        if(!pasta.equals("")&&!pasta.equals(" ")){            
             caminho = caminho.concat("/" + pasta);
         }
         
-        System.out.println("" + caminho + " :");
+        if(pasta.equals(" ")){
+            System.out.println("" + caminho + "/" + " :");     
+        }else{
+            System.out.println("" + caminho + " :");
+        }
+        
         
         //Imprimindo e guardando em um array os nós desta arvore que são diretorios,portanto estes devem ser visitados
         dirsAvisitar = auxPrintArvoreRec(raiz);
